b2baa9c7ad5a9a526d150ac97703411c2f8a33f0
IAmGroots <hadiseptian.crb@gmail.com>
2023-05-24 17:10:05 +0800

    Tugas 1 PBO

---
+++ src/pkg2109106058_muhammad_hadiseptian_nursukamto_tugas1pbo/User.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pkg2109106058_muhammad_hadiseptian_nursukamto_tugas1pbo;
+
+/**
+ *
+ * @author Hadiseptian
+ */
+public class User {
+    String username;
+    String password;
+    
+    public User(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
